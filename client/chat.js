a89d1449f5ae9d3f1f850ff8478d03479192eb96
Nicholas Zhang <nicholasz2510@gmail.com>
2021-04-22 20:25:58 -0600

    Refactored names to chat + small js convention change

--- client/chat.js
+++ client/chat.js
@@ -1,6 +1,6 @@
 function newMatch() {
-    const dingSound = new Audio("ding.wav");
-    dingSound.play()
+    const DING = new Audio("ding.wav");
+    DING.play()
     document.getElementById("successIndicator").innerHTML = "Success!";
     setTimeout(() => {document.getElementById("successIndicator").innerHTML = ""}, 1000)
 }
